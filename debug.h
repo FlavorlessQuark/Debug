f109d386183986bac96ad9508d9ac4aadc2c930d
Jjosephi <31075794+FlavorlessQuark@users.noreply.github.com>
2020-11-22 13:40:15 -0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -16,13 +16,10 @@
 
 _HIDDEN_ void _intp		(int *ptr,	size_t len)	{size_t i; i = 0;while (i < len) {printf("%d\n", ptr[i]); i++;};}
 _HIDDEN_ void _boolp	(bool *ptr, size_t len)	{size_t i; i = 0;while (i < len) {ptr[i] == true ? printf("True\n") : printf("False\n"); i++;}}
-_HIDDEN_ void _vectp	(_V2 **ptr, size_t len)	{size_t i; i = 0; while (i < len) {printf("(%d,%d)\n", ptr[i]->x, ptr[i]->y); i++;}}
-_HIDDEN_ void _nodep	(_Node *ptr,size_t len)	{while (ptr != NULL){printf("%s\n", ptr->data);ptr = ptr->next;}}
 _HIDDEN_ void _defp		(void *ptr, size_t len)	{_DEBUG_("PRINTING WITH UNDEFINED TYPE (likely char *, use printf instead)");}
 
 
 # define _PRINT_1D(array, size) _Generic((array),\
 	int		*: _intp_  ((int  *) array, size),\
 	bool	*: _boolp_ ((bool *) array, size),\
-	_Node	*: _nodep_ ((_Node *)array, size),\
 	default	 : _defp_  (array, size))
